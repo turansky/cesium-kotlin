d9dba4be2b59b093c779a36f2bcc6535dfc9c995
Victor Turansky <victor.turansky@gmail.com>
2021-03-10 21:08:11 +0300

    Fix function name calculation

--- buildSrc/src/main/kotlin/com/github/turansky/cesium/Declaration.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/Declaration.kt
@@ -3,8 +3,12 @@ package com.github.turansky.cesium
 internal abstract class Declaration {
     protected abstract val source: Definition
 
+    // use minBy instead
     val fileName: String by lazy {
-        source.body.substringBefore(" ")
+        sequenceOf(
+            source.body.substringBefore(" "),
+            source.body.substringBefore("(")
+        ).sortedBy { it.length }.first()
     }
 
     open fun toCode(): String =
