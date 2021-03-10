42856997fef9ad6b7c99e397637d8a9f47e1c53a
Victor Turansky <victor.turansky@gmail.com>
2021-03-11 02:31:52 +0300

    Fix static body check

--- buildSrc/src/main/kotlin/com/github/turansky/cesium/Class.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/Class.kt
@@ -5,7 +5,6 @@ internal class Class(
 ) : TypeBase(source) {
     override val typeName: String = "class"
     override var companion: HasMembers? = null
-    override val staticBody: Boolean = true
 
     companion object {
         const val PREFIX = "export class "
--- buildSrc/src/main/kotlin/com/github/turansky/cesium/Namespace.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/Namespace.kt
@@ -5,6 +5,7 @@ internal class Namespace(
 ) : TypeBase(source) {
     override val typeName: String = "object"
     override val companion: HasMembers? = null
+    override val staticBody: Boolean = true
 
     companion object {
         const val PREFIX = "export namespace "
