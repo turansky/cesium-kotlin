c1cba9cefc713fe93c6cba8a17b570cab787e1d9
Victor Turansky <victor.turansky@gmail.com>
2021-03-10 17:43:21 +0300

    Declarations parsing in separate file

---
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/Declarations.kt
@@ -0,0 +1,38 @@
+package com.github.turansky.cesium
+
+import java.io.File
+
+private val FACTORY_MAP = mapOf(
+    Function.PREFIX to ::Function,
+    Enum.PREFIX to ::Enum,
+    Type.PREFIX to ::Type,
+    "interface " to ::Interface,
+    Interface.PREFIX to ::Interface,
+    Class.PREFIX to ::Class,
+    "namespace " to ::Namespace,
+    Namespace.PREFIX to ::Namespace
+)
+
+internal fun parseDeclarations(
+    definitionsFile: File
+): List<Declaration> {
+    return definitionsFile.readText()
+        .replace("\n}/**", "\n}\n\n/**")
+        .removePrefix("""declare module "cesium" {""")
+        .substringBefore("\n\n\n\n}")
+        .splitToSequence("\n\n/**")
+        .filter { it.isNotBlank() }
+        .asSequence()
+        .map { "/**$it" }
+        .flatMap { it.split("\n\nexport ").asSequence() }
+        .map { parseTopDefinition(it) }
+        .map { source ->
+            val body = source.body
+            val prefix = FACTORY_MAP.keys
+                .first { body.startsWith(it) }
+
+            val newSource = source.copy(body = body.removePrefix(prefix))
+            FACTORY_MAP.getValue(prefix)(newSource)
+        }
+        .toList()
+}
--- buildSrc/src/main/kotlin/com/github/turansky/cesium/Generator.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/Generator.kt
@@ -2,17 +2,6 @@ package com.github.turansky.cesium
 
 import java.io.File
 
-private val FACTORY_MAP = mapOf(
-    Function.PREFIX to ::Function,
-    Enum.PREFIX to ::Enum,
-    Type.PREFIX to ::Type,
-    "interface " to ::Interface,
-    Interface.PREFIX to ::Interface,
-    Class.PREFIX to ::Class,
-    "namespace " to ::Namespace,
-    Namespace.PREFIX to ::Namespace
-)
-
 internal fun generateKotlinDeclarations(
     definitionsFile: File,
     sourceDir: File
@@ -20,24 +9,8 @@ internal fun generateKotlinDeclarations(
     val cesiumDir = sourceDir.resolve("cesium")
         .also { it.mkdirs() }
 
-    definitionsFile.readText()
-        .replace("\n}/**", "\n}\n\n/**")
-        .removePrefix("""declare module "cesium" {""")
-        .substringBefore("\n\n\n\n}")
-        .splitToSequence("\n\n/**")
-        .filter { it.isNotBlank() }
+    parseDeclarations(definitionsFile)
         .asSequence()
-        .map { "/**$it" }
-        .flatMap { it.split("\n\nexport ").asSequence() }
-        .map { parseTopDefinition(it) }
-        .map { source ->
-            val body = source.body
-            val prefix = FACTORY_MAP.keys
-                .first { body.startsWith(it) }
-
-            val newSource = source.copy(body = body.removePrefix(prefix))
-            FACTORY_MAP.getValue(prefix)(newSource)
-        }
         .sortedBy(Declaration::fileName)
         .forEachIndexed { index, declaration ->
             val id = index.toString()
