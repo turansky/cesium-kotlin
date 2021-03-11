b1701404cd6697a7f1692fd929d61c8ff67c731c
Victor Turansky <victor.turansky@gmail.com>
2021-03-11 21:29:29 +0300

    Add remote sources support

--- buildSrc/src/main/kotlin/cesium-declarations.gradle.kts
+++ buildSrc/src/main/kotlin/cesium-declarations.gradle.kts
@@ -5,12 +5,15 @@ tasks {
         delete("src")
     }
 
+    val sourceDir = file("src/main/kotlin")
+    val remoteProjectDir = rootDir.parentFile.resolve("cesium-declarations/cesium-kotlin")
+    val remoteSourceDir = remoteProjectDir.resolve("src/main/kotlin")
+
     val generateDeclarations by registering {
         dependsOn(":kotlinNpmInstall")
 
         doLast {
-            val sourceDir = file("src/main/kotlin")
-                .also { delete(it) }
+            delete(sourceDir)
 
             val definitionsFile = rootProject.buildDir
                 .resolve("js/node_modules/cesium")
@@ -23,7 +26,27 @@ tasks {
         }
     }
 
+    val cleanRemote by registering(Delete::class) {
+        onlyIf {
+            remoteProjectDir.exists()
+        }
+
+        delete(remoteProjectDir.resolve("src"))
+    }
+
+    val updateRemote by registering(Copy::class) {
+        onlyIf {
+            remoteProjectDir.exists()
+        }
+
+        from(sourceDir)
+        into(remoteSourceDir)
+
+        dependsOn(cleanRemote)
+    }
+
     named("compileKotlinJs") {
         dependsOn(generateDeclarations)
+        finalizedBy(updateRemote)
     }
 }
