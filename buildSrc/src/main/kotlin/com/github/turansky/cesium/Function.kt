cdd2e3f3858b63bbc0ebe811220e991d266abd71
Victor Turansky <victor.turansky@gmail.com>
2021-03-12 01:49:20 +0300

    Function generation

--- buildSrc/src/main/kotlin/com/github/turansky/cesium/Function.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/Function.kt
@@ -3,11 +3,17 @@ package com.github.turansky.cesium
 internal class Function(
     override val source: Definition
 ) : Declaration() {
+    private val name = source.parseFunctionName()
+    private val parameters = source.parseFunctionParameters()
+    private val returnType = source.parseFunctionReturnType()
+
     override fun toCode(): String {
+        val returnExpression = returnType?.let { ": $it" } ?: ""
+
         return DEFAULT_PACKAGE +
                 source.doc +
                 "\n\n" +
-                "external val $fileName: Function<dynamic>"
+                "external fun $name ${parameters.toCode()}$returnExpression"
     }
 
     companion object {
