667e17eb59aff8d3376b97be1c0a2efe97e80b9e
Victor Turansky <victor.turansky@gmail.com>
2021-03-13 03:14:25 +0300

    Remove `spans`s and empty `p`s from docs

--- buildSrc/src/main/kotlin/com/github/turansky/cesium/KDoc.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/KDoc.kt
@@ -1,6 +1,8 @@
 package com.github.turansky.cesium
 
 private val DIV_REGEX = Regex("""\n<div .+?\n</div>""", RegexOption.DOT_MATCHES_ALL)
+private val SPAN_1_REGEX = Regex("""\n<span .+?</span>""", RegexOption.DOT_MATCHES_ALL)
+private val SPAN_2_REGEX = Regex("""<span .+?</span>\n""", RegexOption.DOT_MATCHES_ALL)
 
 internal fun kdoc(doc: String): String {
     if (doc.isEmpty())
@@ -13,6 +15,9 @@ internal fun kdoc(doc: String): String {
         .map { it.removePrefix(" ") }
         .joinToString("\n")
         .replace(DIV_REGEX, "")
+        .replace(SPAN_1_REGEX, "")
+        .replace(SPAN_2_REGEX, "")
+        .replace("\n<p>\n</p>", "")
 
     return source
         .splitToSequence("\n")
