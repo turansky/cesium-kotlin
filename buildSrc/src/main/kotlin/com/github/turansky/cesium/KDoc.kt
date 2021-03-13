f6063db2d0773e0ddb6f042d123f770251abd526
Victor Turansky <victor.turansky@gmail.com>
2021-03-13 03:33:15 +0300

    Remove `p`s blocks

--- buildSrc/src/main/kotlin/com/github/turansky/cesium/KDoc.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/KDoc.kt
@@ -3,6 +3,7 @@ package com.github.turansky.cesium
 private val DIV_REGEX = Regex("""\n<div .+?\n</div>""", RegexOption.DOT_MATCHES_ALL)
 private val SPAN_1_REGEX = Regex("""\n<span .+?</span>""", RegexOption.DOT_MATCHES_ALL)
 private val SPAN_2_REGEX = Regex("""<span .+?</span>\n""", RegexOption.DOT_MATCHES_ALL)
+private val P_REGEX = Regex("""<p>(.+?)</p>""", RegexOption.DOT_MATCHES_ALL)
 
 internal fun kdoc(doc: String): String {
     if (doc.isEmpty())
@@ -18,6 +19,9 @@ internal fun kdoc(doc: String): String {
         .replace(SPAN_1_REGEX, "")
         .replace(SPAN_2_REGEX, "")
         .replace("\n<p>\n</p>", "")
+        .replace(P_REGEX, "$1")
+        .replace("\n\n\n", "\n\n")
+        .trim()
 
     return source
         .splitToSequence("\n")
