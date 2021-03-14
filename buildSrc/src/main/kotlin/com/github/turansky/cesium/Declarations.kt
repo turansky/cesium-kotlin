15d991960e695f557286df3b36121bb9d75b50d2
Victor Turansky <victor.turansky@gmail.com>
2021-03-15 02:53:04 +0300

    Workarounds for constructor options

--- buildSrc/src/main/kotlin/com/github/turansky/cesium/Declarations.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/Declarations.kt
@@ -69,6 +69,9 @@ private fun readDeclarations(
         .replace("($TS_FUNCTION)", JS_FUNCTION)
         .replace(TS_FUNCTION, JS_FUNCTION)
         .replace("* /**", "*")
+        .replace("PropertyBag | {\n            [key: string]: any;\n        }", "PropertyBag")
+        .replace("PropertyBag | {\n            [key: string]: TranslationRotationScale;\n        }", "PropertyBag")
+        .replace("PropertyBag | {\n            [key: string]: number;\n        }", "PropertyBag")
         .splitToSequence("\n\n/**")
         .filter { it.isNotBlank() }
         .map { "/**$it" }
