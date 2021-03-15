eae3d5d40ac48c9d88eb4a435092f297c70bf083
Victor Turansky <victor.turansky@gmail.com>
2021-03-15 07:45:16 +0300

    Add JS module annotation

--- buildSrc/src/main/kotlin/com/github/turansky/cesium/Generator.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/cesium/Generator.kt
@@ -2,6 +2,8 @@ package com.github.turansky.cesium
 
 import java.io.File
 
+private const val MODULE_ANNOTATION: String = """@file:JsModule("cesium")"""
+
 internal fun generateKotlinDeclarations(
     definitionsFile: File,
     sourceDir: File
@@ -14,11 +16,16 @@ internal fun generateKotlinDeclarations(
         .sortedBy(Declaration::name)
         .forEach { declaration ->
             val file = cesiumDir.resolve("${declaration.name}.kt")
+            val code = declaration.toCode()
             if (!file.exists()) {
-                file.writeText(declaration.toCode())
+                val content = if ("\nexternal " in code) {
+                    MODULE_ANNOTATION + "\n\n" + code
+                } else code
+
+                file.writeText(content)
             } else {
                 // for functions with union type parameters
-                file.appendText("\n\n" + declaration.toCode())
+                file.appendText("\n\n" + code)
             }
         }
 }
